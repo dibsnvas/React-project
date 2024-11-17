52d9aa4a0a6db95d70c7590199dd0677e260c047
dibsnvas <bndiana12@gmail.com>
2024-11-17 17:51:47 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import "./App.css";
+
+const CyberPage = () => {
+  return (
+    <div className="bg-cover bg-center min-h-screen text-white overflow-hidden">
+      <header className="text-center bg-opacity-70 bg-blue-900 flex items-center space-x-4 h-[80px] w-screen">
+        <img
+          src="/image.png"
+          alt="Cyber Zhauynger Logo"
+          className="h-16 w-16 ml-4"
+        />
+        <h1 className="text-3xl font-bold text-white">
+          Cyber Zhauynger.kz
+        </h1>
+      </header>
+
+      <div className="bg-blue-900 bg-opacity-70 shadow-lg rounded-lg p-6 mb-8 text-center mt-24">
+        <h2 className="text-4xl font-semibold mb-4 text-white">
+          Кибер-Жауынгер
+        </h2>
+        <p className="text-white">прикладная школа инженерии</p>
+        <div className="h-48 bg-gray-300 mt-6 flex items-center justify-center">
+          <span className="text-gray-500">Видео на заднем фоне</span>
+        </div>
+      </div>
+
+      <section className="relative flex items-center justify-center mt-[54px] mb-8">
+  <h3 className="absolute text-3xl font-semibold top-[-2rem] text-white">
+    Выберите интересующее вас направление:
+  </h3>
+  <div className="circular-pyramid">
+    <button className="pyramid-item">Ракетостроение</button>
+    <button className="pyramid-item">Робототехника</button>
+    <button className="pyramid-item">3D печать</button>
+    <button className="pyramid-item">3D моделирование</button>
+    <button className="pyramid-item">Разработка приложений</button>
+  </div>
+</section>
+
+
+      <section className="bg-blue-900 bg-opacity-70 shadow-lg rounded-lg p-6 mb-8">
+        <h3 className="text-xl font-semibold mb-4 text-white">
+          Миссия школы:
+        </h3>
+        <p className="text-white">
+          Школа «Кибер Жауынгер» — это воплощение детской мечты, вдохновлённой
+          образом Тони Старка, гениального изобретателя и защитника. Увлечение
+          его деятельностью побудило меня изучать науку и IT, а трёхлетний опыт
+          работы в образовании и ведение онлайн-школы по олимпиадной физике
+          привели к созданию места, где готовят будущих «Железных людей».
+          Наблюдая ежедневные новости из горячих точек планеты, где
+          политические конфликты между странами всё чаще сопровождаются
+          использованием передовых технологий, мы видим, как дроны, умные
+          системы, ракеты и другие инновации становятся решающим фактором. Это
+          подчеркивает важность подготовки инженеров и IT-специалистов,
+          которые играют ключевую роль в обеспечении безопасности и развитии
+          страны. Мы не только обучаем этим направлениям, но и помогаем
+          ученикам освоить навыки заработка в этих сферах. На курсе по GameDev
+          мы учим загружать созданные игры на платформы, такие как Play Market
+          и App Store. На курсе по 3D-моделированию показываем, как размещать
+          модели на платных платформах для продажи в видеоиграх. А на курсе по
+          3D-печати обучаем создавать изделия, которые можно продавать в
+          качестве пластиковых товаров.и т.д. Как и Тони Старк, применявший свои
+          изобретения для защиты, мы вдохновляем учеников создавать технологии,
+          которые служат миру и обществу. Наша миссия — подготовить поколение
+          инженеров и разработчиков, способных строить мирное и безопасное
+          будущее.
+        </p>
+      </section>
+
+      <footer className="bg-blue-900 bg-opacity-70 shadow-lg rounded-lg p-6 text-center">
+        <p className="text-white">Адрес: Аль-Фараби, 21/7</p>
+        <p className="text-white">
+          Контактные данные: 8 778 873 96 17 (whatsapp)
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default CyberPage;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': '#2563eb'
+      }
+    },
+  },
+  plugins: [],
+};
